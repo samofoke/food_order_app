e8d770006b12bb4e31afa8ea7b86fb2eb2a52efc
samofoke <sabataernestmofokeng@gmail.com>
2023-07-10 20:51:04 +0200

    building the cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
+import { Fragment } from "react";
+import Header from "./Components/header/header";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started.</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
---
+++ src/Components/Button/button.js
@@ -0,0 +1,7 @@
+import classes from "./button.module.css";
+
+const Button = (props) => {
+  return <div></div>;
+};
+
+export default Button;
